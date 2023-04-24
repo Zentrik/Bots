ddc9fd1106911ce73bfefc98fe71078ed585fc05
Zentrik <Zentrik@users.noreply.github.com>
2023-04-24 09:52:08 +0100

    Comment out logging

--- src/Arb.jl
+++ src/Arb.jl
@@ -878,15 +878,15 @@ function testLogging()
 end
 
 # Need to run manually
-using Logging, LoggingExtras, Dates
-timestamp_logger(logger) = TransformerLogger(logger) do log
-    merge(log, (; message = "$(Dates.format(now(), "yyyy-mm-dd HH:MM:SS.sss")) $(log.message)"))
-end
-const DIR = "Bots\\src\\logs"
-global_logger(TeeLogger(
-    EarlyFilteredLogger(Bots.not_Bots_message_filter, ConsoleLogger(stderr, Logging.Info)), 
-    EarlyFilteredLogger(Bots.not_Bots_message_filter, MinLevelLogger(FileLogger("$DIR\\$(Dates.format(now(), "YYYY-mm-dd HH-MM")).log"), Logging.Info)),
-    EarlyFilteredLogger(Bots.not_Bots_message_filter, MinLevelLogger(FileLogger("$DIR\\Debug-$(Dates.format(now(), "YYYY-mm-dd HH-MM")).log"), Logging.Debug)),
-    timestamp_logger(MinLevelLogger(FileLogger("$DIR\\Verbose-$(Dates.format(now(), "YYYY-mm-dd HH-mm")).log"), Logging.Debug))
-))
+# using Logging, LoggingExtras, Dates
+# timestamp_logger(logger) = TransformerLogger(logger) do log
+#     merge(log, (; message = "$(Dates.format(now(), "yyyy-mm-dd HH:MM:SS.sss")) $(log.message)"))
+# end
+# const DIR = "Bots\\src\\logs"
+# global_logger(TeeLogger(
+#     EarlyFilteredLogger(Bots.not_Bots_message_filter, ConsoleLogger(stderr, Logging.Info)), 
+#     EarlyFilteredLogger(Bots.not_Bots_message_filter, MinLevelLogger(FileLogger("$DIR\\$(Dates.format(now(), "YYYY-mm-dd HH-MM")).log"), Logging.Info)),
+#     EarlyFilteredLogger(Bots.not_Bots_message_filter, MinLevelLogger(FileLogger("$DIR\\Debug-$(Dates.format(now(), "YYYY-mm-dd HH-MM")).log"), Logging.Debug)),
+#     timestamp_logger(MinLevelLogger(FileLogger("$DIR\\Verbose-$(Dates.format(now(), "YYYY-mm-dd HH-mm")).log"), Logging.Debug))
+# ))
 # ArbBot.testLogging()
