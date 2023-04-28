c378d3b4e225da1aad107626bc46d2936bfd0f9c
Zentrik <Zentrik@users.noreply.github.com>
2023-04-28 17:12:05 +0100

    Better error printing

--- src/Bots.jl
+++ src/Bots.jl
@@ -2,17 +2,49 @@ module Bots
 
 export ArbBot, Extremities
 
+# https://github.com/julia-vscode/julia-vscode/blob/ed2fb65b42d11b6beec51aaaa4703824cfa0fd1c/scripts/packages/VSCodeServer/src/repl.jl#L233
+function display_error(err, bt)
+    io = IOBuffer()
+    st = stacktrace(crop_backtrace(bt))
+    showerror(IOContext(io, :limit => true), err, st)
+    @error String(take!(io))
+end
+
+# https://github.com/julia-vscode/julia-vscode/blob/ed2fb65b42d11b6beec51aaaa4703824cfa0fd1c/scripts/packages/VSCodeServer/src/misc.jl#L6
+function crop_backtrace(bt)
+    i = find_first_topelevel_scope(bt)
+    return bt[1:(i === nothing ? end : i)]
+end
+
+function find_first_topelevel_scope(bt::Vector{<:Union{Base.InterpreterIP,Ptr{Cvoid}}})
+    for (i, ip) in enumerate(bt)
+        st = Base.StackTraces.lookup(ip)
+        ind = findfirst(st) do frame
+            linfo = frame.linfo
+            if linfo isa Core.CodeInfo
+                linetable = linfo.linetable
+                if isa(linetable, Vector) && length(linetable) ≥ 1
+                    lin = first(linetable)
+                    if isa(lin, Core.LineInfoNode) && lin.method === Symbol("top-level scope")
+                        return true
+                    end
+                end
+            else
+                return frame.func === Symbol("top-level scope")
+            end
+        end
+        ind === nothing || return i
+    end
+    return
+end
+
 macro spawn_showerr(ex)
     esc(quote
         Threads.@spawn try
             eval($ex)
         catch err
-            @error "Something went wrong" err
-            # https://github.com/JuliaLang/julia/pull/48282#issuecomment-1426083522
-            for line in ["[$ii] $frame" for (ii, frame) in enumerate(stacktrace(catch_backtrace()))]
-                @error line
-            end
-            rethrow(err)
+            display_error(err, catch_backtrace())
+            rethrow()
         end
     end)
 end
@@ -22,11 +54,7 @@ macro async_showerr(ex)
         @async try
             eval($ex)
         catch err
-            @error "Something went wrong" err
-            # https://github.com/JuliaLang/julia/pull/48282#issuecomment-1426083522
-            for line in ["[$ii] $frame" for (ii, frame) in enumerate(stacktrace(catch_backtrace()))]
-                @error line
-            end
+            display_error(err, catch_backtrace())
             rethrow()
         end
     end)
@@ -39,11 +67,7 @@ macro smart_assert_showerr(ex, msg=nothing)
         try
             @smart_assert eval($ex) eval($msg)
         catch err
-            @error "Something went wrong" err
-            # https://github.com/JuliaLang/julia/pull/48282#issuecomment-1426083522
-            for line in ["[$ii] $frame" for (ii, frame) in enumerate(stacktrace(catch_backtrace()))]
-                @error line
-            end
+            display_error(err, catch_backtrace())
             rethrow()
         end
     end)
